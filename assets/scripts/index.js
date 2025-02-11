c525f9978480ebb6bb16d39d1b7a9a251409f12f
Jan Biernacik <jan@biernacik.org>
2025-02-11 23:53:29 +0100

    German Site Release 1.0

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,15 @@
-console.log("Hello, user!")
+console.log("Hello, user!");
+let menuToggled = 0;
+
+function toggleMenu() {
+    if (menuToggled == 0) {
+        document.getElementById("menu").style.display = "block";
+        document.getElementById("menuCollapser").style.visibility = "visible";
+        menuToggled = 1;
+    }
+    else {
+        document.getElementById("menu").style.display = "none";
+        document.getElementById("menuCollapser").style.visibility = "hidden";
+        menuToggled = 0;
+    }
+}
