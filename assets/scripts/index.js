847c13f7120dc4275413965472ca4d46c8d69d46
Jan Biernacik <jan@biernacik.org>
2025-02-19 18:08:47 +0100

    It works. I don't know why, I don't know how. But it works.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,59 @@
 console.log("Hello, user!");
 let menuToggled = 0;
 
+$(document).ready(function(){
+    var documentLanguage = document.documentElement.lang;
+    switch(documentLanguage) {
+        case 'de-DE':
+        case 'de-AT':
+        case 'de-CH':
+        case 'de-BE':
+        case 'de-LI':
+        case 'de-LU':
+        case 'de':
+            $(".navbar").load("/de/shared/navbar.html");
+            $(".menu").load("/de/shared/menu.html");
+            break;
+        
+        default:
+            $(".navbar").load("/en/shared/navbar.html");
+            $(".menu").load("/en/shared/menu.html");
+            break;
+    }
+
+    window.addEventListener('popstate', function() {
+        loadContent(window.location.pathname);
+    });
+    bindAjax();
+});
+
+function bindAjax() {
+    $(".ajax-button").click(function(event){
+        event.preventDefault();
+        var pageUrl = $(this).attr("href");
+        loadContent(pageUrl);
+    });
+}
+
+function loadContent(url) {
+    $.ajax({
+        url: url,
+        success: function(response) {
+            var tempDiv = $('<div>').html(response);
+            var title = tempDiv.find('title').text();
+            var pageContent = tempDiv.find(".page-content").html();
+            $(".page-content").html(pageContent);
+            $('html, body').scrollTop(0);
+            $('title').text(title);
+            window.history.replaceState(null, title, url);
+            bindAjax();
+        },
+        error: function() {
+            console.log("Error loading page: " + url);
+        }
+    });
+}
+
 function toggleMenu() {
     if (menuToggled == 0) {
         document.getElementById("menu").style.display = "block";
