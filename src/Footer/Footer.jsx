15c8d0c44eb309f91c764818c47cb0a695774989
Jan Biernacik <jan@biernacik.dev>
2025-03-17 23:28:55 +0100

    Mobile Fix

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,10 +2,17 @@ import { Link } from "react-router";
 
 function Footer() {
     const privacyName = navigator.language === "de-DE" ? "Datenschutz" : "Privacy";
+    const websiteInfo = navigator.language === "de-DE" ? (
+        <p>Diese Website wurde in <a className="bold">Vite</a> geschrieben und wird auf <a className="bold">GitHub Pages</a> gehostet. Das Design basiert auf der <a className="bold">Gruvbox</a>-Farbpalette.</p>
+    ) : (
+        <p>This Website was written in <a className="bold">Vite</a> and is being hosted on <a className="bold">GitHub Pages</a>. The design is based on the <a className="bold">Gruvbox</a> color palette.</p>
+    );
 
     return (
         <div className="main-section">
-            <Link className="link" to="/privacy">{privacyName}</Link>
+            <a className="bold link block" href="https://github.com/JBNCK/janbiernacik.com">Quelltext</a>
+            <Link className="bold link" to="/privacy">{privacyName}</Link>
+            {websiteInfo}
         </div>
     );
 }
