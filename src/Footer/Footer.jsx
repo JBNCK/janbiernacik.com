1227af0d628692940a76e86428c04628df28a788
Jan Biernacik <jan@biernacik.dev>
2025-03-18 10:02:42 +0100

    Remove Website Info

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,17 +3,16 @@ import { Link } from "react-router";
 function Footer() {
     const sourceCodeLink = navigator.language === "de-DE" ? "Quelltext" : "Source Code";
     const privacyName = navigator.language === "de-DE" ? "Datenschutz" : "Privacy";
-    const websiteInfo = navigator.language === "de-DE" ? (
+    /*const websiteInfo = navigator.language === "de-DE" ? (
         <p>Diese Website wurde in <a className="bold">Vite</a> geschrieben und wird auf <a className="bold">GitHub Pages</a> gehostet.</p>
     ) : (
         <p>This Website was written in <a className="bold">Vite</a> and is being hosted on <a className="bold">GitHub Pages</a>.</p>
-    );
+    );*/
 
     return (
         <div className="main-section">
             <a className="bold link block" href="https://github.com/JBNCK/janbiernacik.com">{sourceCodeLink}</a>
             <Link className="bold link" to="/privacy">{privacyName}</Link>
-            {websiteInfo}
         </div>
     );
 }
